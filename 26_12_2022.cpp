692dce86c5aaa8e4b1d05b0b7e65c45984fe2d9b
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2022-12-26 23:47:34 +0530

    Update 26_12_2022.cpp

--- 26_12_2022.cpp
+++ 26_12_2022.cpp
@@ -1,9 +1,5 @@
-
 class Solution{
 public:
-    
-typedef long long ll;
-
     void precompute()
     {
         // Code Here
@@ -11,27 +7,16 @@ typedef long long ll;
     
     long long solve(long long l, long long r){
         // Code Here
-        ll ans=0;
-        for(int i=0;i<63;i++)
-        {
-            for(int j=i+1;j<63;j++)
-            {
-                for(int k=j+1;k<63;k++)
-                {
-                    long long num=0;
-                    num|=(1L<<i);
-                    num|=(1L<<j);
-                    num|=(1L<<k);
-                    
-                    if(num>=l and num<=r)
-                        ans++;
+        long long count=0;
+        for(int i=0;i<63;i++){
+            for(int j=i+1;j<63;j++){
+                for(int k=j+1;k<63;k++){
+                    long long temp=pow(2,i)+pow(2,j)+pow(2,k);
+                    if(temp>=l && temp<=r)
+                    count++;
                 }
             }
         }
-        return ans;
+        return count;
     }
-    
 };
-
-
-
