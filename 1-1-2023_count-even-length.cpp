e7ae0befadced862c771bb3c94aed269cd20e625
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2023-01-01 16:21:01 +0530

    Update 1-1-2023_count-even-length.cpp

--- 1-1-2023_count-even-length.cpp
+++ 1-1-2023_count-even-length.cpp
@@ -1,21 +1,21 @@
 class Solution{
 	public:
+	int mod=1000000007;
+	long long inv(long long int r)
+	{
+	    if(r==1)
+	    return 1;
+	    return (mod-((mod/r)*inv(mod%r))%mod+mod)%mod;
+	}
 	int compute_value(int n)
 	{
 	    // Code here
-	    long long ncr = 1;
-        long long sum = 1;
-        long long m = 1000000007;
-    
-        for(int i=1; i<=n; i++){
-    
-            ncr = ((n+1-i) *ncr)/i;
-            sum = sum + (ncr*ncr);
-            
-            sum = sum;
-        }
-        
-      return sum;
-
+	    long long int ans=1,ncr=1;
+	    for(int i=1;i<=n;i++)
+	    {
+	        ncr=(((ncr*(n+1-i))%mod)*inv(i))%mod;
+	        ans=(ans+(ncr*ncr)%mod)%mod;
+	    }
+	    return ans%mod;
 	}
 };
